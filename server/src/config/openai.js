810ed19124f2ff8e87668cce18bba2e1629a48ee
Lucas Ramos <lucas.ramos.pessoa@hotmail.com>
2024-12-12 21:30:26 -0300

    configuring OpenAI

---
+++ server/src/config/openai.js
@@ -0,0 +1,23 @@
+import OpenAI from 'openai';
+
+module.exports = class openai {
+  static configuration() {
+    const openai = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+
+    return openai;
+  }
+
+  static textCompletion({ prompt }) {
+    return {
+      model: 'gpt-4o-mini',
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 3500,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    };
+  }
+};
