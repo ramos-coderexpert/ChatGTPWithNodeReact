266a374d4628259ecf76c6a5f48c4f7e3dac5f87
Lucas Ramos <lucas.ramos.pessoa@hotmail.com>
2024-12-27 20:59:30 -0300

    Implementing Chat

--- web/src/App.js
+++ web/src/App.js
@@ -3,14 +3,29 @@ import './styles/App.css';
 import './styles/reset.css';
 import { makeRequest } from './api/api';
 import { SideMenu } from './components/SideMenu/SideMenu';
+import { ChatMessage } from './components/ChatMessage';
 
 function App() {
   const [input, setInput] = useState('');
   const [chatLog, setChatLog] = useState([{ user: 'gpt', message: 'Como posso te ajudar hoje?' }]);
 
+  async function handleSubmit(e) {}
+
   return (
     <div className="App">
       <SideMenu></SideMenu>
+      <section className="chatbox">
+        <div className="chat-log">
+          {chatLog.map((message, index) => (
+            <ChatMessage key={index} message={message} />
+          ))}
+        </div>
+        <div className="chat-input-holder">
+          <form onSubmit={handleSubmit}>
+            <input className="chat-input-textarea" value={input} rows="1" onChange={e => setInput(e.target.value)} />
+          </form>
+        </div>
+      </section>
       <h1>App Works!!</h1>
     </div>
   );
